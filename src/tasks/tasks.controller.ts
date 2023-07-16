3a3441dfdd4ab04e8726b866128abd397fd5f2ad
moviefan322 <philman202@yahoo.com>
2023-07-16 10:26:15 -0400

    protects delete from deleting other users tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -55,7 +55,10 @@ export class TasksController {
   }
 
   @Delete('/:id')
-  async deleteTask(@Param('id') id: string): Promise<void> {
-    return this.tasksService.deleteTask(id);
+  async deleteTask(
+    @Param('id') id: string,
+    @GetUser() user: User,
+  ): Promise<void> {
+    return this.tasksService.deleteTask(id, user);
   }
 }
