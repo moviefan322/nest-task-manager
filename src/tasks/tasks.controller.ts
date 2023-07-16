cfebcaf5bc9bd11d54f274b0f0698adabb25caf3
moviefan322 <philman202@yahoo.com>
2023-07-15 21:30:49 -0400

    uses validation via global pipes

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -20,7 +20,7 @@ export class TasksController {
   @Get()
   getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
     if (Object.keys(filterDto).length) {
-      return this.tasksService.getTasksWithFilters(filterDto); 
+      return this.tasksService.getTasksWithFilters(filterDto);
     } else {
       return this.tasksService.getAllTasks();
     }
