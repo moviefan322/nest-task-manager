7e92670c0488a27e74f4dd817fcbbc0a3fa7d401
moviefan322 <philman202@yahoo.com>
2023-07-17 08:47:52 -0400

    applies custom nest logs

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { TransformInterceptor } from './transform.interceptor';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3000);
+  const PORT = 3000;
+  await app.listen(PORT);
+  logger.log(`Application listening on port ${PORT}`);
 }
 bootstrap();
